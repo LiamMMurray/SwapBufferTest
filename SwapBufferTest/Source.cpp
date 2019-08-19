d8d7705c6a0be328a273a82ad3def7052153aed0
Liam <lmmurray@student.fullsail.edu>
2019-08-19 17:49:00 -0400

    structures added

--- SwapBufferTest/Source.cpp
+++ SwapBufferTest/Source.cpp
@@ -1,93 +1,113 @@
-#include <atomic>
-#include <iostream>
-#include <thread>
-#include <new>
 #include <Windows.h>
 #include <assert.h>
-unsigned previous_read = -1;
+#include <atomic>
+#include <iostream>
+#include <new>
+#include <thread>
+unsigned g_dbg_previousRead = -1;
 
-std::thread g_readThread;
-volatile bool g_shutdownReadThread = false;
-volatile bool g_shutdownWriteThread = false;
-constexpr uint64_t g_BUFFER_SIZE = 16384ULL;
-constexpr unsigned g_NUM_BUFFERS = 2;
-
-volatile uint64_t* buffers = (volatile uint64_t*)_aligned_malloc(g_BUFFER_SIZE * sizeof(uint64_t) * g_NUM_BUFFERS, 8);
-volatile uint64_t buffer_sizes[g_NUM_BUFFERS];
-
-unsigned read_buffer_index = 0;
-unsigned write_buffer_index = 1;
-
-void Swap()
+// single producer single consumer 
+struct InputBuffer
 {
-	read_buffer_index = write_buffer_index;
-	write_buffer_index = (write_buffer_index + 1) & (g_NUM_BUFFERS-1);
-}
+    private:
+        static constexpr uint64_t m_BUFFER_SIZE = 16384ULL;
+        static constexpr unsigned m_NUM_BUFFERS = 2;
 
-bool Try_Push_Write(uint64_t value)
+        volatile uint64_t* m_buffers;
+        volatile uint64_t  m_bufferSizes[m_NUM_BUFFERS];
+        unsigned           m_readBufferIndex  = 0;
+        unsigned           m_writeBufferIndex = 1;
+
+        void Swap()
+        {
+                m_readBufferIndex  = m_writeBufferIndex;
+                m_writeBufferIndex = (m_writeBufferIndex + 1) & (m_NUM_BUFFERS - 1);
+        }
+
+        bool TryPushWrite(uint64_t value)
+        {
+                if (m_bufferSizes[m_writeBufferIndex] == m_BUFFER_SIZE)
+                        return false;
+                assert(m_bufferSizes[m_writeBufferIndex] < m_BUFFER_SIZE);
+                m_buffers[m_writeBufferIndex * m_BUFFER_SIZE + m_bufferSizes[m_writeBufferIndex]] = value;
+                m_bufferSizes[m_writeBufferIndex]++;
+                return true;
+        }
+
+    public:
+        void ProcessReads()
+        {
+                auto sz = m_bufferSizes[m_readBufferIndex];
+                if (!sz)
+                        return;
+
+                for (unsigned i = 0; i < m_bufferSizes[m_readBufferIndex]; i++)
+                {
+                        unsigned thisRead = m_buffers[m_readBufferIndex * m_BUFFER_SIZE + i];
+                        assert(thisRead == g_dbg_previousRead + 1);
+                        std::cout << m_buffers[m_readBufferIndex * m_BUFFER_SIZE + i] << "\n";
+                        g_dbg_previousRead = thisRead;
+                }
+
+                m_bufferSizes[m_readBufferIndex] = 0;
+        }
+
+        void Write(uint64_t value)
+        {
+                if (!TryPushWrite(value))
+                {
+                        while (m_bufferSizes[m_readBufferIndex] != 0)
+                        {
+                                Sleep(0);
+                        }
+                        // TODO // Run a job while we wait?
+
+                        //
+                        Swap();
+                        TryPushWrite(value);
+                        return;
+                };
+                if (m_bufferSizes[m_readBufferIndex] == 0)
+                        Swap();
+
+                Sleep(0);
+        }
+
+        void Initialize()
+        {
+                m_buffers = (volatile uint64_t*)_aligned_malloc(m_BUFFER_SIZE * sizeof(uint64_t) * m_NUM_BUFFERS, 8);
+        }
+
+        void Shutdown()
+        {
+                _aligned_free(const_cast<uint64_t*>(m_buffers));
+        }
+};
+
+std::thread   g_readThread;
+volatile bool g_shutdownReadThread  = false;
+bool          g_shutdownWriteThread = false;
+InputBuffer   g_InputBuffer;
+
+int readMain()
 {
-	if (buffer_sizes[write_buffer_index] == g_BUFFER_SIZE)
-		return false;
-	assert(buffer_sizes[write_buffer_index] < g_BUFFER_SIZE);
-	buffers[write_buffer_index * g_BUFFER_SIZE + buffer_sizes[write_buffer_index]] = value;
-	buffer_sizes[write_buffer_index]++;
-	return true;
-}
-
-void Process_Reads()
-{
-	auto sz = buffer_sizes[read_buffer_index];
-	if (!sz)
-		return;
-
-	for (unsigned i = 0; i < buffer_sizes[read_buffer_index]; i++)
-	{
-		unsigned this_read = buffers[read_buffer_index * g_BUFFER_SIZE + i];
-		assert(this_read == previous_read + 1);
-		std::cout << buffers[read_buffer_index * g_BUFFER_SIZE + i] << "\n";
-		previous_read = this_read;
-	}
-
-	buffer_sizes[read_buffer_index] = 0;
-}
-
-int read_main()
-{
-	while (!g_shutdownReadThread)
-	{
-		Process_Reads();
-	}
-	return 0;
+        while (!g_shutdownReadThread)
+        {
+                g_InputBuffer.ProcessReads();
+        }
+        return 0;
 }
 
 int main()
 {
-	uint64_t counter = 0;
-	g_readThread = std::thread(read_main);
-
-	while (counter < 16000000)
-	{
-		if (!Try_Push_Write(counter))
-		{
-			while (buffer_sizes[read_buffer_index] != 0)
-			{
-				Sleep(0);
-			}
-			// TODO // Run a job while we wait?
-
-			//
-			Swap();
-			Try_Push_Write(counter);
-			counter++;
-			continue;
-		};
-		if (buffer_sizes[read_buffer_index] == 0)
-			Swap();
-
-		Sleep(0);
-		counter++;
-	}
-	g_shutdownReadThread = true;
-	g_readThread.join();
-	return 0;
+        g_InputBuffer.Initialize();
+        g_readThread   = std::thread(readMain);
+        unsigned count = 0;
+        while (!g_shutdownWriteThread)
+        {
+                g_InputBuffer.Write(count);
+                count++;
+        }
+        g_InputBuffer.Shutdown();
+        return 0;
 }
